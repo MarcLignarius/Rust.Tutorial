8b4dd2343f95516a249306aec648addb2cf13c7f
Marc Davies <marcdaviesriot@gmail.com>
2020-04-09 14:39:59 -0700

    add placeholder traits example

--- src/print.rs
+++ src/print.rs
@@ -6,9 +6,19 @@ pub fn run() {
   println!("Number: {}", 1);
   println!("{} is from {}", "Marc", "Bordeaux");
 
-  //Positional Arguments
+  // Positional Arguments
   println!(
     "{0} is from {1} and {0} likes to {2}",
     "Marc", "Bordeaux", "code"
   );
+
+  // Named Arguments
+  println!(
+    "{name} likes to play {activity}",
+    name = "Marc",
+    activity = "poker"
+  );
+
+  // Placeholder traits
+  println! ("Binary: {:b} Hex: {:x} Octal {:o}", 10, 10, 10);
 }
